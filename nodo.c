b82e8bdd6a1fac531becb7b2b8b6ecf37dc3363a
IsmaaelMunozAz <izzmael95@gmail.com>
2020-11-30 12:05:21 +0100

    esqueleto bien

--- nodo.c
+++ nodo.c
@@ -5,12 +5,16 @@
 #include <linux/cdev.h>
 #include <linux/moduleparam.h>
 #include <linux/device.h>
-
+#include <linux/slab.h>
+#include <linux/seq_file.h>
+#include <linux/vmalloc.h>
+#include <linux/pagemap.h>
 
 
 MODULE_LICENSE("Dual BSD/GPL");
 
 #define MENOR 1
+#define BUF_LEN 2048
 #define KPMSIZE sizeof(u64)
 
 static int minor = MENOR;
@@ -25,20 +29,42 @@ struct cdev my_cdev[MENOR];
 
 
 static int stat_seq_open(struct inode *inode, struct file *filp) {
-	/*unsigned int *ptr;
-	struct seq_file *file_open;
-    unsigned size=4096*(1+num_possible_cpus()/32);
-	pr_info("%d\n",size);
-	ptr = kmalloc(size, GFP_KERNEL);
-	if (!ptr)
+	char *buf;
+	pr_info("antes del kmalloc cuando cambio");
+	buf = kmalloc(BUF_LEN*sizeof(char), GFP_KERNEL);
+	pr_info("Deberiamos ver algo aqui :%p\n",&buf);
+	if (!buf){
 		pr_info("Error allocating memory\n");
-	file_open=filp->private_data;
-	file_open->private=ptr;
-	kfree(ptr);*/
+		return -ENOMEM;
+	}else{
+		buf[0]='h';
+		buf[1]='o';
+		buf[2]='l';
+		buf[3]='a';
+		buf[4]='\n';
+	}
+	pr_info("antes de colocar el puntero\n");
+	filp->private_data=buf;
 	return 0;
+	//struct file *file_open;
+    //unsigned size=4096*(1+num_possible_cpus()/32);
+	//pr_info("%d\n",size);
+	/*pr_info("antes de meter private_data\n");
+	file_open=filp->private_data;*/
+	/*pr_info("antes de colocar el puntero:buf\n");
+	file_open->buf=buf;
+	pr_info("antes de colocar el size\n");
+	file_open->size=BUF_LEN;*/
+	/*pr_info("antes de colocar el puntero\n");
+	file_open->private=ptr;
+	pr_info("Antes del kfree\n");*/
+	
 }
 
 static int stat_seq_release(struct inode *inode, struct file *filp) {
+	pr_info("Antes del kfree\n");
+	kfree(filp->private_data);
+	pr_info("Finalizo el release\n");
     return 0;
 }
 
@@ -47,18 +73,41 @@ static ssize_t stat_seq_write(struct file *filp, const char __user *buf, size_t 
 }
 
 static ssize_t stat_seq_read(struct file *filp, char __user *buf, size_t count, loff_t *f_pos) {
-	/*u64 __user *out = (u64 __user *)buf;
+	char *origen=filp->private_data;
+	char *destino=buf;
+	size_t i=0;
+	size_t pos=*f_pos;
+	for(;i<count;i++){
+		char *dir=origen+pos+i;
+		pr_info("dir=%p && i=%ld && pos=%ld && origen=%p && destino=%p\n",dir, i, pos,origen,destino+i);
+		if(put_user(*dir,destino+i))
+			return -EFAULT;	
+		char c;	
+		if(get_user(c,destino+i))
+			pr_info("puto error de los cojons\n");
+		pr_info("linea=%c\n",c);
+	}
+	*f_pos+=i;
+	return i;
+	/*long pfn;
+	u64 __user *out = (u64 __user *)buf;
 	u64 pcount;
-	struct page *ppages=NULL;
+	struct page *ppages;
 	long src=*f_pos;
-	long pfn;
-
+	pr_info("entramos en el read\n");
+	pr_info("%ld\n",src);
+	pr_info("calculamos tam pagina\n");
 	pfn=src/KPMSIZE;
-	count=min_t(size_t,count, (max_pfn*KPMSIZE)-src);
-	if(src & KPMSIZE || count & KPMSIZE)
+	pr_info("%ld\n",pfn);
+	//count=min_t(size_t,count, (max_pfn * KPMSIZE) - src);
+	if(src & KPMSIZE || count & KPMSIZE){
+		pr_info("Error -EINVAL\n");
 		return -EINVAL;
+
+	}
 	while(count>0){
-		if(pfn_valid(pfn)){
+		/*if(pfn_valid(pfn)){
+			pr_info("Antes del paginado\n");
 			ppages=pfn_to_page(pfn);
 		}
 		pfn++;
@@ -66,6 +115,7 @@ static ssize_t stat_seq_read(struct file *filp, char __user *buf, size_t count, 
 			pcount=0;
 		}
 		else{
+			//pr_info("Antes del mapeado de las paginas\n");
 			pcount=page_mapcount(ppages);
 		}
 		if(put_user(pcount,out++)){
@@ -74,8 +124,7 @@ static ssize_t stat_seq_read(struct file *filp, char __user *buf, size_t count, 
 		count=count-KPMSIZE;
 	}
 	*f_pos += (char __user *)out - buf;*/
-
-    return 0;
+    
 }
 
 static struct file_operations seq_fops = {
@@ -84,6 +133,7 @@ static struct file_operations seq_fops = {
         .release =  stat_seq_release,
         .write =    stat_seq_write,
         .read =     stat_seq_read
+
 };
 
 static int nodo_init(void){
